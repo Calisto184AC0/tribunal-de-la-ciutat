f62f4a186869d1d678d25fecce79b9ce8e1fb3ab
Calisto184AC0 <arturorajch@gmail.com>
2023-01-27 02:46:32 +0100

    va otra

--- frontend/src/components/_pages/Carrusel/hooks/useCarrusel.tsx
+++ frontend/src/components/_pages/Carrusel/hooks/useCarrusel.tsx
@@ -13,7 +13,7 @@ const useCarrusel = () => {
                     method: 'GET',
                     headers: {
                         Authorization:
-                            'Bearer bec529958f1f94ce19e763a77254d2f65c800b47ab08bd256a5c771694b03e4dff0361aa6c54babb0faa4835c9c12e053409604d335b502021ca8576009a3d6ef7217a55fa6f2533b06e1bd76e1f16caf58c02e5eb45c37946e053ca110c403b449675f8ab344731ba3c7da8fab3324a1d353ca36eee971c20f46409139d2e3d',
+                            'Bearer 776aedf04518019ca4c83b230efbb451ce469f3ea52566ccef3cc0b9ec92668dc1e586bbec1c915262727242be7de57b078ec3ed1deeadb3706d23f59e52d8c3e55b8e232a2047b7cc55218477c9da63e4db4d700a85b7aa59c087bb75dd5d7e3627f9bf5d20d5d20b2d51be382704a7e48a36db333b3f0ed34891081a94b170',
                     },
                 }
             )
