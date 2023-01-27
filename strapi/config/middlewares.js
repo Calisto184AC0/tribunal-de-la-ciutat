884da261ca81fccda249953fb4da840ac2f8ac9a
Calisto184AC0 <arturorajch@gmail.com>
2023-01-27 02:49:05 +0100

    por si aca

--- strapi/config/middlewares.js
+++ strapi/config/middlewares.js
@@ -5,7 +5,6 @@ module.exports = [
     name: "strapi::cors",
     config: {
       origin: "*",
-      maxAge: 86400,
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
